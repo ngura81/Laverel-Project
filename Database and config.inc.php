83cbd839de3be3ca8a9281fda102c80f1bfb177b
ngura81 <46022515+ngura81@users.noreply.github.com>
2020-04-22 22:24:39 +0300

    Update Database and config.inc.php

--- Database and config.inc.php
+++ Database and config.inc.php
@@ -3,6 +3,10 @@ mysql> USE mysql;
 mysql> UPDATE user SET password=PASSWORD("NEWPASSWORD") WHERE User='root';
 mysql> FLUSH PRIVILEGES;
 mysql> quit
+
+login to database
+mysql -u root -p root;
+enter: password
  
  
  
@@ -10,3 +14,9 @@ mysql> quit
   sudo nano config.inc.php
   change false
   remove password
+
+
+php artisan cache:clear
+php artisan route:clear
+php artisan config:clear
+php artisan view:clear
